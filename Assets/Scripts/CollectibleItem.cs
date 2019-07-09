7f06955f8104cbd5d4c5530fe1fbbc553ceb9a5b
Ivan Kelber <ivankelber@gmail.com>
2019-07-09 10:47:35 -0400

    added items

---
+++ Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+    [SerializeField] private ItemId id;
+
+    private void OnTriggerEnter(Collider other) {
+        Destroy(this.gameObject);
+    }
+}
